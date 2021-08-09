ca90397f8a30e1065e9a88fe49497c0de3648371
Bruno Nowajenstu <bnowajenstu@gmail.com>
2021-08-09 11:32:58 -0400

    comentario

--- db/migrate/20210806163443_create_reviews.rb
+++ db/migrate/20210806163443_create_reviews.rb
@@ -1,8 +1,8 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
-      t.integer :rating
-      t.text :content
+      t.integer :rating, null: false
+      t.text :content, null: false
       t.references :restaurant, null: false, foreign_key: true
 
       t.timestamps
